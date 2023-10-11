a2d1ca168150858c5aa6e1fefb848574f618a6d1
FluffyTowels <63123880+FluffyTowels@users.noreply.github.com>
2023-10-11 19:31:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Math;
+namespace square
+{
+    class Prog
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 1;
+            do
+            {
+                Console.WriteLine($"{a} {Sqrt(a)}");
+                a--;
+            }
+            while (a >= b);
+        }
+    }
+}
