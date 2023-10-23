aa9c78dd9ef9e08acab676fe924b7db677e2a559
FluffyTowels <63123880+FluffyTowels@users.noreply.github.com>
2023-10-23 20:54:18 +0700

    семинары 20.10

---
+++ C#/Homework/20.10.23/20.10-1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace doubleloop
+{
+    class Prog
+    {
+        static void Main()
+        {
+            Console.WriteLine(MaxDivInterval(1, 100));
+            RectList(60, true);
+            Console.WriteLine(NatRow(2, 5));
+        }
+
+        static int MaxDivInterval(int a, int b)         //Найти натуральное число из интервала от a до b с максимальной суммой делителей.
+        {
+            int maxdivsum = 0;
+            int otpt = 0;                                   
+            int divsum = 0;
+            for (int i = a; i <= b; i++)
+            {
+                divsum = 0;
+                for (int j = 1; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        divsum += j;
+                    }
+                    if (divsum > maxdivsum)
+                    {
+                        maxdivsum = divsum;
+                        otpt = i;
+                    }
+                }
+            }
+            return otpt;
+        }
+        static void RectList (int S, bool a)                        // Найти размеры всех прямоугольников,
+        {                                                           // площадь которых равна заданному натуральному числу s и стороны которых выражены натуральными числами.
+            if (a)                                                  // При этом решения, которые получаются перестановкой размеров сторон:
+            {                                                       // а) считать разными;
+                for (int i = 1; i <= S; i++)                        // б) считать совпадающими. 
+                {
+                    if (S % i == 0)
+                    {
+                        Console.WriteLine($"a = {i}; b = {S / i}");
+                    }
+                }
+            }
+            else                                                    //б
+            {
+                int bmin = 10000;
+                for (int i = 1; i < bmin; i++)
+                {
+                    if (S / i < bmin)
+                    {
+                        bmin = S / i;
+                    }
+                    if (S % i == 0)
+                    {
+                        Console.WriteLine($"a = {i}; b = {S / i}");
+                    }
+                }
+            }
+        }
+        static int NatRow (int n, int m)                            //Даны натуральные числа m и n. Вычислить 1n + 2n + … + mn.
+        {
+            int otpt = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                otpt += i * n;
+            }
+            return otpt;
+        }
+    }
+}
---
+++ C#/Homework/20.10.23/20.10-2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace LoopsnIfs
+{
+    class Prog
+    {
+        static void Main()
+        {
+            int[] A = CreateRandArray(30);
+            Console.WriteLine(ThirtyNum(A));
+            Console.WriteLine(Street(A));
+        }
+        static int ThirtyNum(int[] A)               // Даны 30 чисел, образующих неубывающую последовательность.
+        {                                           // Найти количество различных чисел в последовательности.
+            int cntr = 1;
+            Array.Sort(A);
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] != A[i-1])
+                {
+                    cntr++;
+                }
+            }
+            return cntr;
+        }
+        static string Street(int[] houses)           // Известно число жителей, проживающих в каждом доме улицы.
+        {                                            // Нумерация домов проведена подряд. 
+            int OddSum = 0;                          // Дома с нечетными номерами расположены на одной стороне улицы, с четными — на другой. 
+            int EvenSum = 0;                         // На какой стороне улицы проживает больше жителей? Использовать только один оператор цикла.
+            for (int i = 0; i < houses.Length; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    EvenSum += houses[i];
+                }         
+                else
+                {
+                    OddSum += houses[i];
+                }
+            }
+            if (EvenSum > OddSum)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+        static int Average (int a)                    // Даны натуральное число m и целые числа a1, a2, …, am.  
+        {                                             // Определить среднее арифметическое тех чисел ai , которые кратны числу n. 
+            return a;                                 // Допустить, что чисел, кратных n, среди заданных может не быть.          
+            // Не понял задание
+        }
+
+        static int[] CreateRandArray(int N)
+        {
+            var rand = new Random();
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = rand.Next(1, 1000);
+            }
+            return A;
+        }
+    }
+}
