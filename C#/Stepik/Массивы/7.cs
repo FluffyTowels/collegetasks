b299d31abaaba531b040f2d4775b52d3c46e25cf
FluffyTowels <63123880+FluffyTowels@users.noreply.github.com>
2023-11-21 19:13:44 +0700

    Update 7.cs

--- C#/Stepik/Массивы/7.cs
+++ C#/Stepik/Массивы/7.cs
@@ -13,7 +13,7 @@ public class MainClass
         int[] nums = new int [cardNumber.Length];
         for (int i = 0; i < cardNumber.Length; i++)
         {
-            nums[i] = Convert.ToInt32(Convert.ToString(cardNumber[i]));
+            nums[i] = Convert.ToInt32(Convert.ToString(cardNumber[i]));     //Можно так же nums[i] = Convert.ToInt32(cardNumber[i] - '0');
         }
         foreach (int i in nums)
         {
