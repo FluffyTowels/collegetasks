9b2a29d3521ad6028bf737941c1382d16712c0f2
FluffyTowels <63123880+FluffyTowels@users.noreply.github.com>
2023-11-22 10:32:26 +0700

    лаба сумма чисел

---
+++ C#/Lab/сумма чисел в строке.cs
@@ -0,0 +1,36 @@
+namespace SumString
+{
+    class Prog
+    {
+        static void Main()
+        {
+            string inpt = "апр12-=   морп345бра    2";
+            string nums = "";
+            bool space = false;
+            int sum = 0;
+            foreach (char c in inpt)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    nums += c;
+                    space = false;
+                }
+                else
+                {
+                    if (!space)
+                    {
+                        nums += " ";
+                        space = true;
+                    }
+                }
+            }
+            nums = nums.Trim();
+            string[] N = nums.Split(' ');
+            foreach (string str in N)
+            {
+                sum += int.Parse(str);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
